a3239183aaf0df1ebfcbee7a2663b9f46df4b093
Rukshan Fernando <ruk.fernando93@gmail.com>
2022-07-09 13:43:17 +0200

    removed unnecessary observer

--- src/features/theme/ThemeProvider.tsx
+++ src/features/theme/ThemeProvider.tsx
@@ -3,17 +3,13 @@ import { ThemeProvider as MuiThemeProvider } from "@mui/material";
 import { ThemeStoreType } from "../../di/types";
 import { ThemeStore } from "../../di/schema";
 import container from "../../di/container";
-import { Observer, observer } from "mobx-react";
+import { observer } from "mobx-react";
 
 const ThemeProvider: React.FC<PropsWithChildren<{}>> = ({ children }) => {
   const themeStore = container.get<ThemeStore>(ThemeStoreType);
 
   return (
-    <Observer>
-      {() => (
-        <MuiThemeProvider theme={themeStore.theme}>{children}</MuiThemeProvider>
-      )}
-    </Observer>
+    <MuiThemeProvider theme={themeStore.theme}>{children}</MuiThemeProvider>
   );
 };
 
